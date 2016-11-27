436f68ab3cb4e2d65ffad3ea24ae7d7502cfc463
NicolasBazan <NicolasBazan.nikobazan@hotmail.com>
2016-11-27 02:13:42 -0300

    js, saldo en todas las paginas

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,64 @@
 function initFirebase(){
-	console.log("iniciando firebase");
-  var config = {
-	apiKey: "AIzaSyAeY5ekmTdQKT5nQj_BFdO_t4l9g1gw1CQ",
-	authDomain: "just-for-now-shop.firebaseapp.com",
-	databaseURL: "https://just-for-now-shop.firebaseio.com",
-	storageBucket: "just-for-now-shop.appspot.com",
-	messagingSenderId: "309599821973",
-	GoogleAuthProvider: "Du1pewHMg125Pl2kN1_r8mu4"
-};
-if (firebase.initializeApp(config)){
-	console.log("Firebase Inicializado")
-} else{
-	console.log(error)
-}
+	console.log("iniciando db");
+	src="https://www.gstatic.com/firebasejs/3.5.3/firebase.js";
+  	var config = {
+		apiKey: "AIzaSyAeY5ekmTdQKT5nQj_BFdO_t4l9g1gw1CQ",
+		authDomain: "just-for-now-shop.firebaseapp.com",
+		databaseURL: "https://just-for-now-shop.firebaseio.com",
+		storageBucket: "just-for-now-shop.appspot.com",
+		messagingSenderId: "309599821973",
+		GoogleAuthProvider: "Du1pewHMg125Pl2kN1_r8mu4"
+	};
 firebase.initializeApp(config);
 var database=firebase.database();
+
+
 }
- 
-  function salario(){
-    console.log("buscando salario");
-    //URl de las notas
-    var qn = firebase.database().ref('usuarios/');
-    //buscamos notas
-    qn.once('value', 
-      function(usuarios){  // Si esta todo bien
-        console.log(usuarios);
+
+
+	function salario(){
+	    console.log("buscando salario");
+	    //URl de las notas
+	    var qn = firebase.database().ref('/usuarios');
+	    //buscamos notas
+	    qn.once('value', 
+	      function(usuarios){  // Si esta todo bien
+	        console.log(usuarios);
+	        var user = firebase.auth().currentUser;
+	        
+	        usuarios.forEach(function(usuario){ 
+	          console.log(usuario.key);
+	                        
+	          if (user.email == usuario.val().email){
+	          document.getElementById("saldo").innerHTML=("<i class='material-icons left'>payment</i> $"+usuario.val().salario)
+	          document.getElementById("nom_user").innerHTML=("<i class='material-icons left'>perm_identity</i> "+usuario.val().usuario)
+	          document.getElementById("mensaje").innerHTML=("Bienvenido "+ usuario.val().usuario)
+
+	          }
+
+
+	        });
+	      },
+	      function(error){ //si hay un error
+	        console.log(error);
+
+	      });
+	  }
+
+  function addCarrito(){
+  		var producto = document.getElementById("precio").value;
+      console.log(producto);
+  		var qn = firebase.database().ref('usuarios/');
+  		qn.once('value', 
+      	function(usuarios){  // Si esta todo bien
         var user = firebase.auth().currentUser;
         
-        usuarios.forEach(function(usuario){ 
-          console.log(usuario.key);
-                        
+        usuarios.forEach(function(usuario){                         
           if (user.email == usuario.val().email){
-          document.getElementById("saldo").innerHTML=("<i class='material-icons left'>payment</i> $"+usuario.val().salario)
-          document.getElementById("nom_user").innerHTML=("<i class='material-icons left'>perm_identity</i> "+usuario.val().usuario)
-          document.getElementById("mensaje").innerHTML=("Bienvenido "+ usuario.val().usuario)
+  			    var saldo = usuario.val().salario;
+  			    console.log(producto + " ..." + saldo);
 
-          }
-
-
-        });
-      },
-      function(error){ //si hay un error
-        console.log(error);
-
-      });
-  }
+  		}
+		});
+	});
+}
